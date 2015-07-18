cfece0424f72cda1a776f8873ec8d711d0308d1c
Craig Carey <craig@Craigs-MacBook-Pro.local>
2015-07-18 23:00:35 +0100

    Up to listing 8.6

--- MyLittleCommunity/router/routes.js
+++ MyLittleCommunity/router/routes.js
@@ -1,3 +1,19 @@
-/**
- * Created by craig on 18/07/15.
- */
+// defines a path and associates it with a function to call if the URL matches ths path
+Router.route('/', function()
+{
+    // renders the specified template
+    this.render('home');
+});
+
+Router.route('/about', function()
+{
+    this.render('about');
+});
+
+Router.route('/profiles/manuel', function()
+{
+    // sets a layout
+    this.layout('profileLayout');
+    // The layout template is used to render the specified profileDetail template
+    this.render('profileDetail');
+});
