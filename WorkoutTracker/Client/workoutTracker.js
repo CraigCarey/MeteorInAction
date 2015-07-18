eed46610f5e51ea118a8a22a7c975328fbdce437
Craig Carey <craig@Craigs-MacBook-Pro.local>
2015-07-18 18:57:19 +0100

    Up to Listing 7.10, pg 172

--- WorkoutTracker/Client/workoutTracker.js
+++ WorkoutTracker/Client/workoutTracker.js
@@ -1,2 +1,25 @@
-// subscribe to the publication of the same name (in server/publications.js)
-Meteor.subscribe("workouts");
+// For the limit parameter of the subscription, a Session variable with a default value of 10 is initialised first
+Session.setDefault('limit', 10);
+
+// Autorun creates a reactive context that renews the subscription if the limit changes.
+Tracker.autorun(function(computation)
+{
+    // subscribe to the publication of the same name (in server/publications.js)
+    Meteor.subscribe('workouts', {
+        // the session object is passed as the value for limit
+        limit: Session.get('limit')
+    });
+});
+
+// event handler to increase the limit by 10
+Template.workoutList.events({
+    'click button.show-more': function(evt, tpl)
+    {
+        var newLimit = Session.get('limit') + 10;
+        // changing the limit in the reactive session variable will update the subscription automatically
+        Session.set('limit', newLimit);
+    }
+});
+
+// gets data from custom aggregation publication
+DistanceByMonth = new Mongo.Collection('distanceByMonth');
--- WorkoutTracker/Server/publications.js
+++ WorkoutTracker/Server/publications.js
@@ -1,6 +1,110 @@
 // Publications are one-way streets that send data from the server to the client
 // publications are named the same as subscriptions
-Meteor.publish('workouts', function () {
-    // publications return data, same as helpers
-    return WorkoutsCollection.find({});
+Meteor.publish('workouts', function(options)
+{
+    // validate the options object parameter, since it is coming from the client
+    check(options,
+        {
+            // ensure that the limit part of options is a number
+            limit: Number
+        }
+    );
+
+    // query for all workouts that belong to the curretly logged in user
+    var qry = {
+        userId: this.userId
+    };
+
+    var qryOptions = {
+        limit: options.limit,
+
+        // Sort all database entries by timestamp to ensure the limit starts from the newest
+        sort: {workoutAt: 1}
+    }
+
+    // Using the limit query options from MongoDB returns only a limited number of documents
+    return WorkoutsCollection.find(qry, qryOptions);
+});
+
+
+Meteor.publish('distanceByMonth', function ()
+{
+    var subscription = this;
+    // used to prevent the first docs from the initial subscription from affecting the added callback
+    var initiated = false;
+    var distances = {};
+    var userId = this.userId;
+
+    // Because there’s no official support for aggregation from Meteor, use the core Mongo driver
+    var db = MongoInternals.defaultRemoteCollectionDriver().mongo.db;
+
+    // The aggregation settings create documents with the _id field equal to the month of the workoutAt field
+    // and the sum of all distances of this month.
+    var pipeline = [
+        {
+            // The aggregation should only be done over documents that match the userId
+            $match: { userId: userId }
+        },
+        {
+        $group: {
+            _id: {
+                $month: '$workoutAt'
+            },
+            distance: {
+                $sum: '$distance'
+            }
+        }
+    }];
+
+    // Creating the aggregation
+    db.collection('workouts').aggregate(
+        pipeline,
+        // Because you can’t use asynchronous code in a publication, use Meteor.bindEnvironment
+        Meteor.bindEnvironment(
+            function (err, result) {
+                console.log('result', result);
+                _.each(result, function (r) {
+                    // Add the data to the subscription
+                    subscription.added('distanceByMonth', r._id, {
+                        distance: r.distance
+                    });
+                })
+            }
+        )
+    )
+
+    var workoutHandle = WorkoutsCollection
+        // In this publication there should only the documents with the logged in userId observed
+        .find({
+            userId: userId
+        })
+        .observeChanges({
+            added: function (id, fields) {
+                if (!initiated) return;
+
+                // create the id of the document (jan = 1...)
+                idByMonth = new Date(fields.workoutAt).getMonth() + 1;
+
+                // update the month for which a new workout was added
+                distances[idByMonth] += fields.distance;
+
+                // inform the client that the subscription has changed
+                subscription.changed('distanceByMonth',
+                    idByMonth, {
+                        distance: distances[idByMonth]
+                    }
+                )
+            }
+        });
+
+    initiated = true;
+
+    // onStop callback fired when the clients subscription is closed
+    subscription.onStop(function () {
+        // The handle that’s returned from the observerChanges() function is used to stop observing.
+        workoutHandle.stop();
+    });
+
+    // the subscription is ready to send to the client
+    subscription.ready();
 });
